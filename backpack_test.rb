334a2403061a2d2220193b4b3648394d1434e5a9
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-31 15:17:20 -0400

    Add 2 tests for presence of gym shoes in backpack items, depending on day of week

--- backpack_test.rb
+++ backpack_test.rb
@@ -45,6 +45,19 @@ class BackpackTest < MiniTest::Test
 
   ###### TESTS ABOUT GYM SHOES... wait, where are the tests about gym shoes? #####
   # is something missing here?
+  def test_that_packs_gym_shoes_on_monday_and_thursday
+    ['monday', 'thursday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      assert(backpack.items.include?('gym shoes'))
+    end
+  end
+
+  def test_that_doesnt_pack_gym_shoes_on_all_other_days
+    ['tuesday', 'wednesday', 'friday', 'saturday', 'sunday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      refute(backpack.items.include?('gym shoes'))
+    end
+  end
 
 
   ##### TESTS ABOUT PRINTING THE LIST #####
